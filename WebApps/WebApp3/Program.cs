1e3b2932d7eb1768a6e124bb812de464ab0a0b97
AbhijitKumarJ <abhijitkumarjha88192@gmail.com>
2026-01-01 01:31:19 +0530

    Add WebApp3 with WeatherForecast API and configure project settings

---
+++ WebApps/WebApp3/Program.cs
@@ -0,0 +1,61 @@
+
+using System.IO;
+//using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.FileProviders;
+using Newtonsoft.Json;
+//using WebApp3.Data;
+
+namespace WebApp3;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
+
+        // Add services to the container.
+
+        builder.Services.AddControllers().AddNewtonsoftJson(options =>
+        {
+            options.SerializerSettings.ReferenceLoopHandling = ReferenceLoopHandling.Ignore;
+            options.SerializerSettings.NullValueHandling = NullValueHandling.Ignore;
+        });
+
+        // Configure EF Core with SQLite file in content root
+        //var dbFile = Path.Combine(builder.Environment.ContentRootPath, "app.db");
+        //var connectionString = $"Data Source={dbFile}";
+        //builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlite(connectionString));
+
+        // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+        builder.Services.AddOpenApi();
+
+        var app = builder.Build();
+
+        // Ensure database exists
+        // using (var scope = app.Services.CreateScope())
+        // {
+        //     var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+        //     db.Database.EnsureCreated();
+        // }
+
+        // Configure the HTTP request pipeline.
+        if (app.Environment.IsDevelopment())
+        {
+            app.MapOpenApi("/openapi/v1.json");
+        }
+
+        app.UseStaticFiles();
+
+        app.UseRouting();
+
+        //app.UseHttpsRedirection();
+
+        app.UseAuthorization();
+
+        app.MapControllers();
+
+        app.MapGet("/", () => Results.Redirect("/index.html"));
+
+        app.Run();
+    }
+}
